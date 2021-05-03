fddbcf28e678b71b0d911c9254f0ee4119ba40eb
Aaron Crighton <acrighton1@gmail.com>
2021-05-02 18:08:42 -0700

    test commit

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+//Get the button
+var mybutton = document.getElementById("myBtn");
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
   } else {
-    navbar.classList.remove("sticky");
+    mybutton.style.display = "none";
   }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 
