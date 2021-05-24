a7488b334cf70ba87d630b9141f0b54251f77d61
Aaron Crighton <acrighton1@gmail.com>
2021-05-23 21:59:55 -0700

    edits

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-//Get the button
-var mybutton = document.getElementById("myBtn");
+var btn = $('#button');
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() 
-{scrollFunction()
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
   } else {
-    mybutton.style.display = "none";
+    btn.removeClass('show');
   }
-}
+});
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
 
 
 const element = document.querySelector('.mainlogo');
