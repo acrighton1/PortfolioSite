05475717aff6d535383015e91e8b25c792dc32fa
Aaron Crighton <acrighton1@gmail.com>
2021-07-21 03:18:34 -0700

    TOTAL SITE REVISION

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+var rootElement = document.documentElement
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
+
+window.onscroll = function() {scrollFunction()};
 
 
 
 const element = document.querySelector('.mainlogo');
-element.classList.add('animate__animated', 'animate__fadeIn');
+element.classList.add('animate__animated', 'animate__backInDown');
+
+
 
 
 
