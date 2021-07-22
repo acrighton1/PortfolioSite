5dd9319fe2ea5e6bdab9b7ac7bfb35a8b865bb1e
Aaron Crighton <acrighton1@gmail.com>
2021-07-21 17:10:40 -0700

    git push

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-var scrollToTopBtn = document.getElementById("scrollToTopBtn")
-var rootElement = document.documentElement
 
-function scrollToTop() {
-  // Scroll to top logic
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
-}
-scrollToTopBtn.addEventListener("click", scrollToTop)
-
-window.onscroll = function() {scrollFunction()};
 
 
 
