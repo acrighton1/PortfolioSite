bd807ef28c1846169f80f59ef2063f2748f26b5c
Aaron Crighton <acrighton1@gmail.com>
2023-01-23 21:40:45 -0700

    fixed click anywhere close overlay issue

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ AOS.init();
 
 
   
-
+ // Close the navbar menu if the user clicks outside of it
+ document.addEventListener("click", function(event) {
+    if (!event.target.matches('.nav-icon') && event.target.matches('.overlay')) {
+      closeNav();
+    }
+  });
 
 
 
